7a1bf8de036e27ced17a4770f63c2c685e300e39
Jean Louw <me@jlouw.co.za>
2016-03-04 11:55:20 +0200

    Added command line arguments
    Check for name

--- NameSplitter.py
+++ NameSplitter.py
@@ -1,46 +1,66 @@
 import csv
+import sys
 
 FILE_PATH = r"\\OLIFANT\Documents\GitHub\LearnPython"
-# NEW_FILE_PATH = r"\\OLIFANT\Documents\GitHub\LearnPython\newnames.txt"
-# YES_FILE_PATH = r"\\OLIFANT\Documents\GitHub\LearnPython\yesVoters.csv"
 
-print("\nRead new name txt file into a LIST:")
-new_file = open(FILE_PATH + r"\newnames.txt")
-new_names = []
 
-for line in new_file.readlines():
-    names_and_surname = line.partition(' ')
-    name = names_and_surname[0]
-    rest = line.replace(name, "").replace('\\n', "").strip()
-    new_names.append([name, rest])
-    print(new_names[-1])
+def check_name(name):
+    return
 
-print("\nRead csv file into a LIST:")
-yes_file = open(FILE_PATH + r"\yesVoters.csv", 'r+')
-yes_names = []
 
-for row in csv.reader(yes_file):
-    yes_names.append(row)
-    print(yes_names[-1])
+def import_names():
 
-print("\nSort the LIST from the csv file:")
-yes_names.sort(key=lambda x: x[1])
+    print("\nRead new name txt file into a LIST:")
+    new_file = open(FILE_PATH + r"\newnames.txt")
+    new_names = []
 
-for row in yes_names:
-    print(row)
+    for line in new_file.readlines():
+        names_and_surname = line.partition(' ')
+        name = names_and_surname[0]
+        rest = line.replace(name, "").replace('\\n', "").strip()
+        new_names.append([name, rest])
+        print(new_names[-1])
 
-print("\nFind diff between csn file LIST and txt file LIST:")
-diff = set(map(tuple, new_names)) - set(map(tuple, yes_names))
-print(("Yes: %i, New: %i, Diff %i") % (len(yes_names), len(new_names), len(diff)))
+    print("\nRead csv file into a LIST:")
+    yes_file = open(FILE_PATH + r"\yesVoters.csv", 'r+')
+    yes_names = []
 
-for row in diff:
-    print(row)
+    for row in csv.reader(yes_file):
+        yes_names.append(row)
+        print(yes_names[-1])
 
-print("\nWrite the unique names to the csv file\n")
-writer = csv.writer(yes_file, delimiter=',', lineterminator='\n')
+    print("\nSort the LIST from the csv file:")
+    yes_names.sort(key=lambda x: x[1])
 
-for newPerson in diff:
-    writer.writerow(newPerson)
+    for row in yes_names:
+        print(row)
 
-new_file.close()
-yes_file.close()
+    print("\nFind diff between csn file LIST and txt file LIST:")
+    diff = set(map(tuple, new_names)) - set(map(tuple, yes_names))
+    print(("Yes: %i, New: %i, Diff %i") % (len(yes_names), len(new_names), len(diff)))
+
+    for row in diff:
+        print(row)
+
+    print("\nWrite the unique names to the csv file\n")
+    writer = csv.writer(yes_file, delimiter=',', lineterminator='\n')
+
+    for newPerson in diff:
+        writer.writerow(newPerson)
+
+    new_file.close()
+    yes_file.close()
+
+'''
+Below here receives input from the command line
+'''
+arguments = str(sys.argv).split()
+
+print("Args: " + str(sys.argv))
+print(arguments)
+
+if (sys.argv[1] == 'import'):
+    print("Here")
+    import_names()
+elif (sys.argv[1] == 'check'):
+    check_name(arguments[2])
